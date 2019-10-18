121de6d2536f7c65ac0dc8d56f77a4035cef9888
mhniu <niuminghong@elibot.cn>
2019-10-18 11:26:00 +0800

    upload

---
+++ src/main/java/Elite/junit/App.java
@@ -0,0 +1,13 @@
+package Elite.junit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Elite/junit/TestFirst.java
@@ -0,0 +1,14 @@
+package Elite.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFirst {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
